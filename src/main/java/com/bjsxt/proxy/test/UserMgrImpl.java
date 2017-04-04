753a66070583bc0d9de0e62a39d09bb362fb0342
alikiller1 <liu2011321@sina.com>
2017-04-04 18:49:01 +0800

    init

---
+++ src/main/java/com/bjsxt/proxy/test/UserMgrImpl.java
@@ -0,0 +1,11 @@
+package com.bjsxt.proxy.test;
+
+public class UserMgrImpl implements UserMgr {
+
+	@Override
+	public void addUser() {
+		System.out.println("开始添加用户");
+		System.out.println("添加用户成功");
+	}
+	
+}
